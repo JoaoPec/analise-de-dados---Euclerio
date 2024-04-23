d1877ac467122402b3126d593d7e3c86219f4245
JoaoPec <Joaopecurcino@hotmail.com>
2024-04-23 19:50:12 -0300

    Remover arquivo do controle de versão

--- writeCsvToExcell/main.py
+++ writeCsvToExcell/main.py
@@ -4,15 +4,12 @@ import csv
 def main():
     with open('enterpriseSurvey.csv', 'r') as file:
 
-        # Ler o arquivo CSV e carregar os dados em um DataFrame do pandas
         reader = csv.reader(file)
-        data = list(reader)  # Converte os dados do leitor CSV em uma lista
+        data = list(reader)
 
-    # Criar um DataFrame do pandas com os dados do CSV
     df = pd.DataFrame(data)
     print(df)
 
-    # Escrever o DataFrame para um arquivo Excel
     df.to_excel('enterpriseData.xlsx', index=False)
 
 main()
